b26fbd222ce32409468d75446645c270fa008a57
MorsDr <178063130+MorsDr@users.noreply.github.com>
2026-03-30 21:10:03 +0300

    Initialize utils file. Change logic of work browser_module file to create more advanced search. Create configuration folder and browser configuration file in json format

---
+++ scripts/utils.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import json
+
+def html_cleaner(raw_text):
+    soup=BeautifulSoup(raw_text, 'lxml')
+    for anchor in soup.find_all("a", class_="headelink"):
+        anchor.decompose()
+    for tag in soup(["script", "style", "nav", "footer", "header"]):
+        tag.decompose()
+    clean_text=soup.get_text(separator=' ')
+    lines=[line.strip() for line in clean_text.splitlines() if line.strip()]
+    return " ".join(lines)
+
+def browser_config():
+    config_path=Path(__file__).resolve().parents[1]/"configs/browser_config.json"
+    try:
+        with open(config_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except json.JSONDecodeError as e:
+        print(f"Ошибка структуры файла конфинурации: {config_path.name} в строке: {e.lineno}")
+    except Exception as e:
+        print(f"Ошибка загрузки конфиг-файла: {e}")
